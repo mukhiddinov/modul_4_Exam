26e66e08bab4bad12ff3d24a194801d3a1c95769
Xusniddin <muxiddinovxusniddin34@gmail.com>
2024-02-21 17:39:27 +0500

    3-misol

--- src/Task3.java
+++ src/Task3.java
@@ -1,5 +1,33 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        String file = "example.txt";
+        String fileResult = "result.txt";
+        File sourceFile = new File(file);
 
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            String firstWrite = "2 10";
+            fileWriter.write(firstWrite);
+            fileWriter.close();
+
+            Scanner scanner = new Scanner(sourceFile);
+            FileWriter fileWriterForResult = new FileWriter(fileResult);
+            while (scanner.hasNextLine()){
+                Integer num1 = scanner.nextInt();
+                Integer num2 = scanner.nextInt();
+                Integer result = num1 + num2;
+                fileWriterForResult.write("Result: " + result);
+            }
+
+            fileWriterForResult.close();
+            System.out.println("file copied!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
