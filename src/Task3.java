37c91dd577cfc0752ed232f0d1a72f2ffe69dfa5
Xusniddin <muxiddinovxusniddin34@gmail.com>
2024-02-21 16:49:24 +0500

    4-savol

---
+++ src/Task3.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String file = "example.txt";
+        String fileCopy = "example_copy.txt";
+        File sourceFile = new File(file);
+
+        try {
+            Scanner scanner = new Scanner(file);
+            FileWriter fileWriter = new FileWriter(fileCopy);
+            while (scanner.hasNextLine()){
+                String lineString = scanner.nextLine();
+                fileWriter.write(lineString + "\n");
+            }
+            fileWriter.close();
+            System.out.println("file copied!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
